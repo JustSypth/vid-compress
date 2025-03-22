055b93b8a16f11dc01266edbeabd897cafc6d350
JustSypth <justsypth@gmail.com>
2025-03-22 15:14:07 +0100

    Patch incorrect window sizing on KDE (Set window size on runtime)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,24 @@
+use tauri::Manager;
+use tauri::Size;
+
 mod commands;
 mod core;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![
-            commands::get_os, commands::get_version, commands::open_url, commands::get_path, commands::begin
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    .plugin(tauri_plugin_opener::init())
+    .setup(|app| {
+        let main_window = app.get_webview_window("main").unwrap();
+        main_window.set_size(Size::Logical(tauri::LogicalSize {
+            width: 402.0,
+            height: 522.0,
+        })).unwrap();
+        Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![
+        commands::get_os, commands::get_version, commands::open_url, commands::get_path, commands::begin
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
