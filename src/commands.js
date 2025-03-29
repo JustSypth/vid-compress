e907decf06810426c82ba18356e1b02f947579e3
JustSypth <justsypth@gmail.com>
2025-03-29 15:37:43 +0100

    Reformat commands.js

--- src/commands.js
+++ src/commands.js
@@ -17,6 +17,21 @@ listen('STATUS', (event) => {
     progressbar.innerHTML = event.payload;
 });
 
+async function begin() {
+    if (processing) {return;}
+    var path = document.getElementById('path_textbox');
+    var crf = document.getElementById('slider');
+    var preset = document.getElementById('preset');
+
+
+    try {
+        await window.__TAURI__.core.invoke('begin', {path: path.value, crf: crf.value, preset: preset.value});
+        console.log('begin(): Succesfully called the backend');
+    } catch (error) {
+        console.error('begin(): Error calling backend:', error);
+    }
+}
+
 async function get_path() {
     try {
         const response = await window.__TAURI__.core.invoke('get_path');
@@ -25,24 +40,6 @@ async function get_path() {
         path.value = response;
     } catch (error) {
         console.log("get_path(): Error: ", error);
-    }
-}
-
-async function begin() {
-    if (processing) {return;}
-    var path = document.getElementById('path_textbox');
-    var cfg = document.getElementById('slider');
-    var preset = document.getElementById('preset');
-
-    // console.log("Path: ", path.value);
-    // console.log("Cfg: ", cfg.value);
-    // console.log("Preset: ", preset.value);
-
-    try {
-        await window.__TAURI__.core.invoke('begin', {path: path.value, cfg: cfg.value, preset: preset.value});
-        console.log('begin(): Succesfully called the backend');
-    } catch (error) {
-        console.error('begin(): Error calling backend:', error);
     }
 }
 
