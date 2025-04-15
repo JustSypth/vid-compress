dffc1d7bd1a92892758024fdd833b6dd6309b3fe
JustSypth <justsypth@gmail.com>
2025-04-15 16:31:57 +0200

    Seperate init.js into frontend and backend

--- src/init.js
+++ src/init.js
@@ -10,25 +10,6 @@ const versionText = document.getElementById('version');
 let overlays = document.getElementsByClassName("overlay");
 let overlays_array = Array.from(overlays)
 
-// Update window borders based on OS
-let osPromise = window.__TAURI__.core.invoke('get_os');
-osPromise.then((os) => {
-    if (os == "windows") {
-        main.style.borderRadius = 0;
-
-        for (let i = 0; i < overlays.length; i++) {
-            overlays[i].style.borderRadius = 0;
-        }
-    }
-});
-
-// Update version value in info overlay
-let versionPromise = window.__TAURI__.core.invoke('get_version');
-versionPromise.then((version) => {
-    console.log("Version: " + version);
-    versionText.innerHTML = "v"+version;
-});
-
 // Update slider value display
 slider.addEventListener("input", () => {
     output.textContent = slider.value;
